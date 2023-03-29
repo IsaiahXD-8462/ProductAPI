fa9f263cab16b676433ca6ba2b6aed29eb12ea60
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-03-29 17:31:06 -0400

    Minor changes

--- products/models.py
+++ products/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=3, decimal_places=2)
+    inventory_quantity = models.IntegerField()
+    visual = models.CharField(max_length=255)
